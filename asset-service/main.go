f1f9434d448e214324c213837cad3776079a8f5a
Thai Le Phan Minh <thailephanminh@gmail.com>
2022-11-09 21:28:51 +0700

    fix: update url path

--- asset-service/main.go
+++ asset-service/main.go
@@ -6,21 +6,15 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/thailephan/lv/asset-service/routes"
-	"net/http"
 )
 
 func main() {
 	router := gin.Default()
-	router.Static("/assets", "./public")
+	router.Static("/", "./public")
 
 	// Set a lower memory limit for multipart forms (default is 32 MiB)
 	//router.MaxMultipartMemory = 8 << 20 // 8 MiB
 
-	router.GET("/hello", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"Hi": "name?",
-		})
-	})
 	routes.SetupRoutes(router)
 	router.Run(":8080")
 }
--- asset-service/routes/index.go
+++ asset-service/routes/index.go
@@ -6,9 +6,9 @@ import (
 )
 
 func SetupRoutes(router *gin.Engine) {
-	router.POST("/messages/upload-attachments", controllers.MessageAttachmentUpload)
+	router.POST("/api/messages/upload-attachments", controllers.MessageAttachmentUpload)
 
 	// TODO: Implement for these below api
-	router.POST("/channels/upload-avatar", controllers.ChannelAvatarUpload)
-	router.POST("/users/upload-avatar", controllers.UserAvatarUpload)
+	router.POST("/api/channels/upload-avatar", controllers.ChannelAvatarUpload)
+	router.POST("/api/users/upload-avatar", controllers.UserAvatarUpload)
 }
