76372a836b6513409c0a3e1c92a80fc6c9c8df69
Thai Le Phan Minh <thailephanminh@gmail.com>
2022-11-09 20:03:33 +0700

    feat:add greeting post

--- asset-service/main.go
+++ asset-service/main.go
@@ -6,6 +6,7 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/thailephan/lv/asset-service/routes"
+	"net/http"
 )
 
 func main() {
@@ -15,6 +16,11 @@ func main() {
 	// Set a lower memory limit for multipart forms (default is 32 MiB)
 	//router.MaxMultipartMemory = 8 << 20 // 8 MiB
 
+	router.POST("/greeting", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"Oke": "abc",
+		})
+	})
 	routes.SetupRoutes(router)
 	router.Run(":8080")
 }
